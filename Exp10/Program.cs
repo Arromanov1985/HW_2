d0b187c4b15d0aa0aa5b332648312af1a6554108
Artem <aromanov.b@ya.ru>
2023-02-18 20:08:23 +0300

    Задача10

--- Exp10/Program.cs
+++ Exp10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число :  ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+//int a = value / 100; // 5  div - /
+int b = value / 10;
+//int c = value % 10;  // 7  mod - %
+
+int result =  b % 10 ;
+Console.WriteLine(result);
